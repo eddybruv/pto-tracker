0f0a92faf32bf392ce71a5f15505cfd641d006fa
Edwin Ajong <edwinajong@gmail.com>
2026-03-14 17:09:21 -0400

    Add root TypeScript and Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
+
+module.exports = config;
